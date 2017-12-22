299417fc0aa10487e09e893f360a02480e0d659a
Lucien Greathouse <me@lpghatguy.com>
2017-12-21 22:26:20 -0800

    Improve codegen by using write (fewer allocations!)

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,17 +1,24 @@
+use std::fmt::Write;
+
 use parser::{AstProgram, AstFunction, AstExpression, AstStatement};
 
-fn generate_statement(statement: &AstStatement) -> String {
+fn generate_statement(statement: &AstStatement, output: &mut String) {
 	match statement {
 		&AstStatement::Return { ref expression } => {
-			format!("movl ${}, %eax\nret", expression.value)
+			write!(output, "movl ${}, %eax\nret", expression.value);
 		},
 	}
 }
 
-fn generate_function(function: &AstFunction) -> String {
-	format!(".globl {}\n{}:\n{}", function.name, function.name, generate_statement(&function.statement))
+fn generate_function(function: &AstFunction, output: &mut String) {
+	write!(output, ".globl {}\n{}:\n", function.name, function.name);
+	generate_statement(&function.statement, output);
 }
 
 pub fn generate_program(program: &AstProgram) -> String {
-	generate_function(&program.function)
+	let mut result = String::new();
+
+	generate_function(&program.function, &mut result);
+
+	result
 }
--- test-compiler.sh
+++ test-compiler.sh
@@ -3,5 +3,7 @@ cargo run > out.s
 gcc -m32 out.s -o out
 ./out
 
+echo "Return code: $?"
+
 rm out.s
 rm out
